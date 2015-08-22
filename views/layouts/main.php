78a75e14daaf7812dd80271157f8df7c0405364b
Dawid Myslak <dawid.myslak@gmail.com>
2015-08-22 22:10:57 +0100

    Small visual changes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Home Budget',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
