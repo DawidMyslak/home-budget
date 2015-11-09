65e1f2c6d28a6dc184eb56310fb6c1f017b113f8
Dawid Myslak <dawid.myslak@gmail.com>
2015-11-09 23:50:22 +0000

    Start page update

--- views/site/index.php
+++ views/site/index.php
@@ -23,37 +23,28 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-sm-4">
                 <h2>Import</h2>
 
                 <p>Import your transactions from any Irish bank.</p>
 
                 <p><a class="btn btn-default" href="">More &raquo;</a></p>
             </div>
-            <div class="col-lg-6">
+            <div class="col-sm-4">
                 <h2>Categorise</h2>
 
                 <p>Automatically categorise your expenses.</p>
 
                 <p><a class="btn btn-default" href="">More &raquo;</a></p>
             </div>
-            <div class="col-lg-6">
+            <div class="col-sm-4">
                 <h2>Analyze</h2>
 
                 <p>Analyze charts and categorized data.</p>
 
                 <p><a class="btn btn-default" href="">More &raquo;</a></p>
             </div>
-            <div class="col-lg-6">
-                <h2>Share</h2>
-
-                <p>Share your data with family or friends.</p>
-
-                <p><a class="btn btn-default" href="">More &raquo;</a></p>
-            </div>
         </div>
-        
-        <br><br>
 
     </div>
 </div>
